396290d0589574ad6998ee13376a50ff4220b0ca
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-06-01 20:38:53 -0500

    addes colums to schema and added menu items to seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+
+DROP DATABASE IF EXISTS taco_truck_db;
+
 CREATE DATABASE taco_truck_db;
 
 USE taco_truck_db;
@@ -5,9 +8,16 @@ USE taco_truck_db;
 CREATE TABLE menu_items
 (
 	id int NOT NULL AUTO_INCREMENT,
-	item varchar(255) NOT NULL,
+	item varchar(20) NOT NULL,
+    shell enum('hard', 'soft') NOT NULL DEFAULT 'hard',
+    shell_type enum('flour', 'corn') NOT NULL DEFAULT 'flour',
+    lettuce enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
+    tomatoes enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
+    cheese enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
+    onion enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
     description varchar(255) NOT NULL,
     price decimal(15,2) NOT NULL,
+    veagan boolean NOT NULL DEFAULT false,
 	PRIMARY KEY (id)
 );
 
@@ -19,7 +29,9 @@ CREATE TABLE taco_orders
     order_id int(10) NOT NULL,
 	taco varchar(10) NOT NULL,
 	customization varchar(255) NOT NULL,
-    sauce varchar(10) NOT NULL,
+    sauce enum('none', 'mild', 'hot', 'fire') NOT NULL DEFAULT 'mild',
+    time_ordered datetime NOT NULL,
+    filled boolean NOT NULL default false,
 	PRIMARY KEY (id),
     FOREIGN KEY (menu_item) REFERENCES menu_items(id)
 );
@@ -30,7 +42,9 @@ CREATE TABLE drink_orders
     menu_item int(20) NOT NULL,
     customer varchar(20) NOT NULL,
     order_id int(10) NOT NULL,
-	drink varchar(10) NOT NULL,
+	drink enum('water', 'coke', 'iced tea', 'lemonade', 'sprite') NOT NULL,
+    time_ordered datetime NOT NULL,
+    filled boolean NOT NULL default false,
 	PRIMARY KEY (id),
     FOREIGN KEY (menu_item) REFERENCES menu_items(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO menu_items (item, description, price) VALUES ('beef taco', 'Taco with farm raised beef that will leave your mouth watering.', '2.50');
+INSERT INTO menu_items (item, description, price) VALUES ('grilled chicken taco', 'Taco with farm raised chicken to perfection.', '2.00');
+INSERT INTO menu_items (item, description, price) VALUES ('fried chicken taco', 'Taco with farm raised chicken air fried perfectly so you can enjoy the crunch without the grease.', '2.00');
+INSERT INTO menu_items (item, description, price) VALUES ('veggie taco', 'Taco made completely with fresh and organic veggies.', '1.50');
+INSERT INTO menu_items (item, description, price) VALUES ('veagan taco', 'Our animal free taco with the great taste you will not believe.', '2.00');
+
