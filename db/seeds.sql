8983ae1a50eeadc91a9b5e2f4e2a19d5c4b46ad9
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-06-03 11:46:37 -0500

    taco and drink menu tables complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,11 @@
-INSERT INTO menu_items (item, description, price) VALUES ('beef taco', 'Taco with farm raised beef that will leave your mouth watering.', '2.50');
-INSERT INTO menu_items (item, description, price) VALUES ('grilled chicken taco', 'Taco with farm raised chicken to perfection.', '2.00');
-INSERT INTO menu_items (item, description, price) VALUES ('fried chicken taco', 'Taco with farm raised chicken air fried perfectly so you can enjoy the crunch without the grease.', '2.00');
-INSERT INTO menu_items (item, description, price) VALUES ('veggie taco', 'Taco made completely with fresh and organic veggies.', '1.50');
-INSERT INTO menu_items (item, description, price) VALUES ('veagan taco', 'Our animal free taco with the great taste you will not believe.', '2.00');
+INSERT INTO taco_menu (item, description, price) VALUES ('beef taco', 'Taco with farm raised beef that will leave your mouth watering.', '2.50');
+INSERT INTO taco_menu (item, description, price) VALUES ('grilled chicken taco', 'Taco with farm raised chicken to perfection.', '2.00');
+INSERT INTO taco_menu (item, description, price) VALUES ('fried chicken taco', 'Taco with farm raised chicken air fried perfectly so you can enjoy the crunch without the grease.', '2.00');
+INSERT INTO taco_menu (item, description, price) VALUES ('veggie taco', 'Taco made completely with fresh and organic veggies.', '1.50');
+INSERT INTO taco_menu (item, description, price) VALUES ('veagan taco', 'Our animal free taco with the great taste you will not believe.', '2.00');
 
+INSERT INTO drink_menu (drink_item, price) VALUES ('water', '.75');
+INSERT INTO drink_menu (drink_item, price) VALUES ('lemonade','1.00');
+INSERT INTO drink_menu (drink_item, price) VALUES ('soda', '1.00');
+INSERT INTO drink_menu (drink_item, price) VALUES ('sweet tea', '1.00');
+INSERT INTO drink_menu (drink_item, price) VALUES ('unsweetened tea', '1.00');
