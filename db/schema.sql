69356d5387e723b8ee3a47fb8a6dcbc6e15997cf
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-06-04 08:26:43 -0500

    consolidated food and drinks on menu tables and items ordered tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,30 +5,23 @@ CREATE DATABASE taco_truck_db;
 
 USE taco_truck_db;
 
-CREATE TABLE taco_menu
+CREATE TABLE menu
 (
 	id int(10) NOT NULL AUTO_INCREMENT,
 	item varchar(20) NOT NULL,
-    shell enum('hard', 'soft') NOT NULL DEFAULT 'hard',
-    shell_type enum('flour', 'corn') NOT NULL DEFAULT 'flour',
-    lettuce enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
-    tomatoes enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
-    cheese enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
-    onion enum('xtra', 'reg', 'light', 'none') NOT NULL DEFAULT 'reg',
-    sauce enum('none', 'mild', 'hot', 'fire') NOT NULL DEFAULT 'mild',
-    description varchar(255) NOT NULL,
+    shell enum('hard', 'soft') DEFAULT 'hard',
+    shell_type enum('flour', 'corn') DEFAULT 'flour',
+    lettuce enum('xtra', 'reg', 'light', 'none') DEFAULT 'reg',
+    tomatoes enum('xtra', 'reg', 'light', 'none') DEFAULT 'reg',
+    cheese enum('xtra', 'reg', 'light', 'none') DEFAULT 'reg',
+    onion enum('xtra', 'reg', 'light', 'none') DEFAULT 'reg',
+    sauce enum('none', 'mild', 'hot', 'fire')  DEFAULT 'mild',
+    description varchar(255),
     price decimal(15,2) NOT NULL,
     veagan boolean NOT NULL DEFAULT false,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE drink_menu
-(
-	id int(10) NOT NULL AUTO_INCREMENT,
-	drink_item enum('water', 'lemonade', 'soda', 'sweet tea', 'unsweetened tea') NOT NULL,
-    price decimal(15,2) NOT NULL,
-	PRIMARY KEY (id)
-);
 
 CREATE TABLE orders
 (
@@ -42,25 +35,14 @@ CREATE TABLE orders
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE tacos_ordered
+CREATE TABLE items_ordered
 (
 	id int(10) NOT NULL AUTO_INCREMENT,
     order_id int(10) NOT NULL,
-    taco_id int(10),
+    item_id int(10),
     quantity int(10) NOT NULL default '1',
     description varchar(255),
 	PRIMARY KEY (id),
     FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (taco_id) REFERENCES taco_menu(id)
-);
-
-CREATE TABLE drinks_ordered
-(
-	id int(10) NOT NULL AUTO_INCREMENT,
-    order_id int(10) NOT NULL,
-    drink_id int(10),
-    quantity int(10) NOT NULL default '1',
-	PRIMARY KEY (id),
-    FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (drink_id) REFERENCES drink_menu(id)
+    FOREIGN KEY (item_id) REFERENCES menu(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,26 @@
-INSERT INTO taco_menu (item, description, price) VALUES ('beef taco', 'Taco with farm raised beef that will leave your mouth watering.', '2.50');
-INSERT INTO taco_menu (item, description, price) VALUES ('grilled chicken taco', 'Taco with farm raised chicken to perfection.', '2.00');
-INSERT INTO taco_menu (item, description, price) VALUES ('fried chicken taco', 'Taco with farm raised chicken air fried perfectly so you can enjoy the crunch without the grease.', '2.00');
-INSERT INTO taco_menu (item, description, price) VALUES ('veggie taco', 'Taco made completely with fresh and organic veggies.', '1.50');
-INSERT INTO taco_menu (item, description, price) VALUES ('veagan taco', 'Our animal free taco with the great taste you will not believe.', '2.00');
+INSERT INTO menu (item, description, price) VALUES ('beef taco', 'Taco with farm raised beef that will leave your mouth watering.', '2.50');
+INSERT INTO menu (item, description, price) VALUES ('grilled chicken taco', 'Taco with farm raised chicken to perfection.', '2.00');
+INSERT INTO menu (item, description, price) VALUES ('fried chicken taco', 'Taco with farm raised chicken air fried perfectly so you can enjoy the crunch without the grease.', '2.00');
+INSERT INTO menu (item, description, price) VALUES ('veggie taco', 'Taco made completely with fresh and organic veggies.', '1.50');
+INSERT INTO menu (item, description, price) VALUES ('veagan taco', 'Our animal free taco with the great taste you will not believe.', '2.00');
 
-INSERT INTO drink_menu (drink_item, price) VALUES ('water', '.75');
-INSERT INTO drink_menu (drink_item, price) VALUES ('lemonade','1.00');
-INSERT INTO drink_menu (drink_item, price) VALUES ('soda', '1.00');
-INSERT INTO drink_menu (drink_item, price) VALUES ('sweet tea', '1.00');
-INSERT INTO drink_menu (drink_item, price) VALUES ('unsweetened tea', '1.00');
+INSERT INTO menu (item, shell, shell_type, lettuce, tomatoes, cheese, onion, sauce, price) 
+VALUES ('drink', null, null, null, null, null, null, null, '1.00');
+
+INSERT INTO orders (customer, price, sales_tax, total_price) VALUES ('Greg', '5.00', '2.00', '7.00');
+INSERT INTO orders (customer, price, sales_tax, total_price) VALUES ('Tina', '3.00', '1.00', '4.00');
+INSERT INTO orders (customer, price, sales_tax, total_price) VALUES ('Paul', '1.50', '.75', '2.25');
+
+INSERT INTO items_ordered (order_id, item_id, description) VALUES ('1', '1', 'test order');
+INSERT INTO items_ordered (order_id, item_id, description) VALUES ('1', '2', 'test order');
+INSERT INTO items_ordered (order_id, item_id, description) VALUES ('2', '3', 'test order');
+INSERT INTO items_ordered (order_id, item_id, description) VALUES ('3', '4', 'test order');
+
+INSERT INTO items_ordered (order_id, item_id) VALUES ('1', '6');
+INSERT INTO items_ordered (order_id, item_id) VALUES ('1', '6');
+INSERT INTO items_ordered (order_id, item_id) VALUES ('2', '6');
+
+
+
+
+
